7ccd4e45fef8649d3f9ac514779e151f925ce36f
Anupam Gupta <c2gupt@gmail.com>
2025-03-31 13:06:19 +0530

    newpage add

--- src/Components/NewPageTest.jsx
+++ src/Components/NewPageTest.jsx
@@ -4,6 +4,10 @@ function NewPageTest() {
     return (
         <div>
             <h1>NewPageTest</h1>
+            <p>This is a test page.</p>
+            <p>Welcome to the new page!</p>
+            <p>Feel free to add more content here.</p>
+            <p>Test the new page functionality.</p>
         </div>
     );
 }
