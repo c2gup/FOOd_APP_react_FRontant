2400e60967451b104b3c0c083e3cfcebdf61c72a
Anupam Gupta <c2gupt@gmail.com>
2025-03-31 12:49:09 +0530

    update new PAge

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import OurFood from './Components/OurFood';
 import Eome from './Components/Home';
 import AboutUs from './Components/AboutUs';
 import SignIn from './Components/SignIn';
+import NewPageTest from './Components/NewPageTest';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
@@ -25,6 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={< Eome handelcounts={handelcount}   />} />
         <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/new" element={<NewPageTest />} />
         <Route path="/ourfood" element={<OurFood handelcounts={handelcount}  />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
---
+++ src/Components/NewPageTest.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewPageTest() {
+    return (
+        <div>
+            <h1>NewPageTest</h1>
+        </div>
+    );
+}
+
+export default NewPageTest;
